265cbc03a34836a4b6786b8b7a77247e90245e69
lcssathler <lcssathler@gmail.com>
2025-05-16 19:05:48 -0300

    add: create word search main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            String path = "data/documento.pdf";
+            String content = Ingestor.readPDF(path);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Indexer indexer = new Indexer();
+            indexer.indexText(content);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite um termo para buscar:");
+            String term = scanner.nextLine();
+            List<Integer> result = indexer.search(term);
+
+            if (result.isEmpty()) {
+                System.out.println("Termo não encontrado.");
+            } else {
+                System.out.println("Termo encontrado nas linhas: " + result);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
         }
     }
 }
